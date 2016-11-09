ad5d709010246cff35e339b4f662f2f0eeda8965
Moyster <oysterized@gmail.com>
2016-11-09 18:44:15 +0100

    rootdir: properly build boot.img rootdir

--- device_m2note.mk
+++ device_m2note.mk
@@ -15,27 +15,39 @@ TARGET_OTA_ASSERT_DEVICE := m2note,m2n,meizu_m2_note
 
 TARGET_PROVIDES_INIT_RC := true
 
+# Custom system/core/rootdir files
 PRODUCT_COPY_FILES += \
     device/meizu/m2note/rootdir/init.rc:root/init.rc \
-    device/meizu/m2note/rootdir/init.target.rc:root/init.target.rc \
-    device/meizu/m2note/rootdir/init.mt6735.rc:root/init.mt6735.rc \
-    device/meizu/m2note/rootdir/init.ssd.rc:root/init.ssd.rc \
-    device/meizu/m2note/rootdir/init.xlog.rc:root/init.xlog.rc \
-    device/meizu/m2note/rootdir/init.usb.rc:root/init.usb.rc \
-    device/meizu/m2note/rootdir/init.mt6735.usb.rc:root/init.mt6735.usb.rc \
-    device/meizu/m2note/rootdir/init.aee.rc:root/init.aee.rc \
-    device/meizu/m2note/rootdir/init.project.rc:root/init.project.rc \
-    device/meizu/m2note/rootdir/init.modem.rc:root/init.modem.rc \
-    device/meizu/m2note/rootdir/init.trace.rc:root/init.trace.rc \
-    device/meizu/m2note/rootdir/fstab.mt6735:root/fstab.mt6735 \
-    device/meizu/m2note/rootdir/fstab.swap:root/fstab.swap \
-    device/meizu/m2note/rootdir/ueventd.rc:root/ueventd.rc \
-    device/meizu/m2note/etc/media_codecs.xml:system/etc/media_codecs.xml \
     device/meizu/m2note/etc/init/audioserver.rc:system/etc/init/audioserver.rc \
     device/meizu/m2note/etc/init/mediacodec.rc:system/etc/init/mediacodec.rc \
     device/meizu/m2note/etc/init/cameraserver.rc:system/etc/init/cameraserver.rc \
-    device/meizu/m2note/etc/init/rild.rc:system/etc/init/rild.rc \
-    device/meizu/m2note/media_profiles.xml:system/etc/media_profiles.xml \
+    device/meizu/m2note/etc/init/rild.rc:system/etc/init/rild.rc
+
+# device rootdir
+PRODUCT_COPY_FILES += \
+    device/meizu/m2note/rootdir/enableswap.sh:root/enableswap.sh \
+    device/meizu/m2note/rootdir/factory_init.project.rc:root/factory_init.project.rc \
+    device/meizu/m2note/rootdir/factory_init.rc:root/factory_init.rc \
+    device/meizu/m2note/rootdir/fstab.charger:root/fstab.charger \
+    device/meizu/m2note/rootdir/fstab.mt6735:root/fstab.mt6735 \
+    device/meizu/m2note/rootdir/init.modem.rc:root/init.modem.rc \
+    device/meizu/m2note/rootdir/init.mt6735.rc:root/init.mt6735.rc \
+    device/meizu/m2note/rootdir/init.mt6735.usb.rc:root/init.mt6735.usb.rc \
+    device/meizu/m2note/rootdir/init.project.rc:root/init.project.rc \
+    device/meizu/m2note/rootdir/init.ssd.rc:root/init.ssd.rc \
+    device/meizu/m2note/rootdir/meta_init.modem.rc:root/meta_init.modem.rc \
+    device/meizu/m2note/rootdir/meta_init.project.rc:root/meta_init.project.rc \
+    device/meizu/m2note/rootdir/meta_init.rc:root/meta_init.rc \
+    device/meizu/m2note/rootdir/ueventd.mt6735.rc:root/ueventd.mt6735.rc \
+    device/meizu/m2note/rootdir/init.recovery.mt6735.rc:root/init.recovery.mt6735.rc
+
+# Media config
+PRODUCT_COPY_FILES += \
+    device/meizu/m2note/etc/media_codecs.xml:system/etc/media_codecs.xml \
+    device/meizu/m2note/media_profiles.xml:system/etc/media_profiles.xml
+
+# Permissions
+PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
@@ -68,10 +80,6 @@ PRODUCT_PACKAGES += \
 #    libmockdrmcryptoplugin \
 #    libaudio-resampler \
 
-
-# RIL
-#PRODUCT_PACKAGES += \
-#    gsm0710muxd
 
 PRODUCT_PACKAGES += \
     Torch \
@@ -111,7 +119,6 @@ PRODUCT_EXTRA_RECOVERY_KEYS += device/meizu/m2note/meizu
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-#$(call inherit-product, build/target/product/aosp_arm64.mk)
 
 ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.secure=0 \
