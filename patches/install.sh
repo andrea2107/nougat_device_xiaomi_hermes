d2b666ab31bec168ba37a59fa9b4bf1b0bb33e2d
Moyster <oysterized@gmail.com>
2016-11-18 16:55:48 +0100

    patches: add minimum ifc_utils Mtk bits Fixes missing ifc symbols : 11-18 15:52:09.316     0   275 F libc    : CANNOT LINK EXECUTABLE "/system/bin/thermal": cannot locate symbol "ifc_set_throttle" referenced by "/system/bin/thermal"...

---
+++ patches/install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo $1
+rootdirectory="$PWD"
+# ---------------------------------
+
+dirs="system/core"
+#dirs="bionic frameworks/av frameworks/base frameworks/native hardware/libhardware system/core system/netd system/vold"
+
+VENDOR=meizu
+DEVICE=m2note
+
+for dir in $dirs ; do
+	cd $rootdirectory
+	cd $dir
+	echo -e "Applying $dir patches...\n"
+	#git am $rootdirectory/device/$VENDOR/$DEVICE/patches/$dir/*.patch
+	git apply $rootdirectory/device/$VENDOR/$DEVICE/patches/$dir/*.patch
+done
+
+# -----------------------------------
+echo -e "Done !\n"
+cd $rootdirectory
+
---
+++ patches/uninstall.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo $1
+rootdirectory="$PWD"
+# ---------------------------------
+
+dirs="system/core"
+#dirs="bionic external/jpeg frameworks/av frameworks/base frameworks/native frameworks/rs frameworks/opt/telephony hardware/libhardware system/core system/netd system/vold"
+
+VENDOR=meizu
+DEVICE=m2note
+
+for dir in $dirs ; do
+	cd $rootdirectory
+	cd $dir
+	echo -e "Cleaning $dir patches...\n"
+	git reset --hard && git clean -df
+done
+
+# -----------------------------------
+echo -e "Done !\n"
+cd $rootdirectory
+
