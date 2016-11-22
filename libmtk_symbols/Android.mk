f11929934165bb09f594769b449882479bb9523c
Mister Oyster <oysterized@gmail.com>
2016-11-22 11:41:13 +0100

    libmtk: use MAD one

--- libmtk_symbols/Android.mk
+++ libmtk_symbols/Android.mk
@@ -2,18 +2,18 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
+    mtk_xlog.cpp \
     mtk_gui.cpp \
     mtk_ui.cpp \
     mtk_omx.cpp \
-    icu55.c
+    mtk_audio.cpp
 
 # only for 32bit libraries
 LOCAL_SRC_FILES_32 := mtk_string.cpp
 # only for 64bit libraries
 LOCAL_SRC_FILES_64 := mtk_parcel.cpp
 
-LOCAL_SHARED_LIBRARIES := libbinder libutils liblog libgui libui \
-                          libicuuc libicui18n
+LOCAL_SHARED_LIBRARIES := libbinder libutils liblog libgui libui
 LOCAL_MODULE := libmtk_symbols
 LOCAL_MODULE_TAGS := optional
 
