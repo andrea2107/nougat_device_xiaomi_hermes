1f6393bbecd7221dce99dda4742bab6846800bda
andrea2107 <andrea210701@gmail.com>
2016-11-24 15:01:54 +0100

    change bluetooth name and fix building

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,7 +1,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Meizu M2Note"
+#define BTM_DEF_LOCAL_NAME   "Xiaomi redmi note 2"
 
 
 /* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
